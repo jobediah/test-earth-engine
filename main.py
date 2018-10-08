da2e68b9dc9645350910903fe01fae795f0eb952
Jody Hansen <jhansen@dri.edu>
2018-10-08 10:31:44 -0700

    Updated to python3.7 and flask

--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-import os
+#!/usr/bin/env python37
 
+import os
 import sys
 
-import webapp2
+from flask import Flask, render_template
 import jinja2
 import config
-#import pycrypto
 import ee
 
-
+app = Flask(__name__)
 
 JINJA_ENVIRONMENT = jinja2.Environment(
                                        loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'),
                                        extensions=['jinja2.ext.autoescape'],
                                        autoescape=True)
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        ee.Initialize(config.EE_CREDENTIALS)
-        template_values = {}
-        #ee.Initialize();
-        
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
-        #self.response.headers['Content-Type'] = 'text/plain'
-        #self.response.write('Hello, World!')
+@app.route('/')
+def MainPage():
+    ee.Initialize(config.EE_CREDENTIALS)
+    template_values = {}
 
+    return render_template('index.html')
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-], debug=True)
